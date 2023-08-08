178267f0fdfd683fe59d08f96b552006b72a7a03
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2023-08-08 00:34:25 -0300

    Create category api and model

---
+++ backend/routes/category.routes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const Category = require("../models/Category");
+
+const CategoryRouter = express.Router();
+
+CategoryRouter.get("/", async (req, res) => {
+  try {
+    const data = await Category.findAll();
+    return res.status(200).json(data);
+  } catch (err) {
+    res.status(400).send(err.toString());
+  }
+});
+
+CategoryRouter.get("/:id", async (req, res) => {
+  try {
+    const data = await Category.findByPk(req.params.id);
+    return res.status(200).json(data);
+  } catch (err) {
+    res.status(400).send(err.toString());
+  }
+});
+
+CategoryRouter.get("*", async (req, res) => {
+  return res.json({ message: "Endpoint não encontrado" });
+});
+CategoryRouter.post("*", async (req, res) => {
+  return res.json({ message: "Endpoint não encontrado" });
+});
+CategoryRouter.put("*", async (req, res) => {
+  return res.json({ message: "Endpoint não encontrado" });
+});
+CategoryRouter.delete("*", async (req, res) => {
+  return res.json({ message: "Endpoint não encontrado" });
+});
+
+module.exports = CategoryRouter;
