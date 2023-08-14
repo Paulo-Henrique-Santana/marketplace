9c32b8ad69c2382181d23b6b596cab49553815e1
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2023-08-13 21:59:11 -0300

    Increases description field limit

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -9,7 +9,7 @@ Product.init(
     name: { type: DataTypes.STRING, allowNull: false },
     price: { type: DataTypes.DECIMAL, allowNull: false },
     quantity: { type: DataTypes.INTEGER, allowNull: false },
-    description: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.STRING(1000), allowNull: false },
     image: { type: DataTypes.STRING, allowNull: false },
   },
   { sequelize, modelName: "Product", timestamps: false }
