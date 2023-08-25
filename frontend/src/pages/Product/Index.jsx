18bdd2fdf52fa970f0800ed799c8b0ffe8058b00
Gabriel Silva <gabrielp_lacerda@hotmail.com>
2023-08-25 13:41:15 -0300

    mouse enter img

--- frontend/src/pages/Product/Index.jsx
+++ frontend/src/pages/Product/Index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useFetch from "../../Hooks/useFetch";
 
@@ -7,6 +7,7 @@ import "./Index.scss";
 const Index = () => {
   const { data, request } = useFetch();
   const { id } = useParams();
+  const [indexImg, setIndexImg] = useState(0);
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -15,17 +16,28 @@ const Index = () => {
     fetchProduct();
   }, [id, request]);
 
-  console.log(data);
 
   return (
     <div className="productDescription">
       {data ? (
         <div className="containerDescription">
           <div className="containerImg">
-            {data.images &&
-              data.images.map((img) => (
-                <img src={`http://localhost:3000/api/files/${img.fileName}`} />
-              ))}
+            <div className="imgs">
+              {data.images &&
+                data.images.map((img, index) => (
+                  <img
+                    src={`http://localhost:3000/api/files/${img.fileName}`}
+                    onMouseEnter={() => setIndexImg(index)}
+                  />
+                ))}
+            </div>
+            <div className="img">
+              {data.images && (
+                <img
+                  src={`http://localhost:3000/api/files/${data.images[indexImg].fileName}`}
+                />
+              )}
+            </div>
           </div>
           <ul>
             <li>{data.name}</li>
