bbbc94e6217160e907b43266ce3d0334a3110b33
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2023-11-09 20:56:29 -0300

    fix(token): fix jwt token validation

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,13 @@
 import cors from "cors";
 import dotenv from "dotenv";
-import express, { json, urlencoded } from "express";
+import express, { Request, json, urlencoded } from "express";
+import jwt from "jsonwebtoken";
 import { sequelize } from "./db/conn";
 import { AuthRouter } from "./routes/auth.routes";
 import { CategoryRouter } from "./routes/category.routes";
 import { FavoriteRouter } from "./routes/favorite.routes";
 import { ProductRouter } from "./routes/product.routes";
 import { UserRouter } from "./routes/user.routes";
-import { verifyJWT } from "./middlewares/verifyJwt";
 
 dotenv.config();
 
@@ -30,11 +30,67 @@ app.use(function (req, res, next) {
   next();
 });
 
+const verifyRouteNeedToken = (req: Request) => {
+  const regex = /\/api\/(.*?)\?/;
+  const route = regex.exec(req.url)?.toString();
+
+  const routesToken = [
+    { name: "user" },
+    { name: "favorite" },
+    { name: "product", methods: ["POST", "PUT", "DELETE"] },
+  ];
+
+  if (route) {
+    return routesToken.some((item) => {
+      if (item.name === route) {
+        if (!item.methods) {
+          return true;
+        } else if (item.methods.includes(req.method)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    });
+  }
+
+  return false;
+};
+
+app.use((req, res, next) => {
+  if (!verifyRouteNeedToken(req)) {
+    return next();
+  }
+
+  if (
+    req.headers.authorization === undefined ||
+    req.headers.authorization.split(" ")[0] !== "Bearer"
+  ) {
+    const status = 401;
+    const message = "Error in authorization format";
+    res.status(status).json({ status, message });
+    return;
+  }
+
+  try {
+    var token = req.headers?.["authorization"]?.split(" ")[1];
+
+    jwt.verify(token!, process.env.JWT_SECRET as string, (err, decoded) => {
+      if (err) return res.status(401).send({ message: "Invalid token" });
+      next();
+    });
+  } catch (err) {
+    const status = 401;
+    const message = "Error access_token is revoked";
+    res.status(status).json({ status, message });
+  }
+});
+
 app.use("/api/auth", AuthRouter);
-app.use("/api/user", verifyJWT, UserRouter);
+app.use("/api/user", UserRouter);
 app.use("/api/product", ProductRouter);
 app.use("/api/category", CategoryRouter);
-app.use("/api/favorite", verifyJWT, FavoriteRouter);
+app.use("/api/favorite", FavoriteRouter);
 app.use("/api/files", express.static("uploads"));
 
 sequelize
